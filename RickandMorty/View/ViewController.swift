a9d78f97d13f48edc8fef1bd6fbce466ba65aabf
Kaan Yıldırım <mkaanyil@gmail.com>
2023-08-08 15:04:35 +0300

    Search Bar Added.

--- RickandMorty/View/ViewController.swift
+++ RickandMorty/View/ViewController.swift
@@ -10,14 +10,23 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var featuresCollectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var characterFeatureList = [Results]()
+    
+    
+    var resultSearch = [Results]()
+    var isSearching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         featuresCollectionView.delegate = self
         featuresCollectionView.dataSource = self
+        
+        searchBar.delegate = self
+        
+        title = "Characters"
         
         let layout = UICollectionViewFlowLayout()
         let width = featuresCollectionView.frame.width
@@ -50,12 +59,11 @@ class ViewController: UIViewController {
                 if let nextURLString = answer.info.next, let nextURL = URL(string: nextURLString) {
                     self.takeData(url: nextURL)
                 }
-                                
-                
+                    
                 /* if let takeCharacterList = answer.results {
                     self.characterFeatureList = takeCharacterList
                 } */
-                
+             
                 DispatchQueue.main.async {
                     self.featuresCollectionView.reloadData()
                 }
@@ -71,18 +79,39 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let index = sender as? Int
         
-        if let destinationVC = segue.destination as? DetailsViewController {
-            destinationVC.characterDetails = characterFeatureList[index!]
+        if isSearching {
+            if let destinationVC = segue.destination as? DetailsViewController {
+                destinationVC.characterDetails = resultSearch[index!]
+            }
+        } else {
+            if let destinationVC = segue.destination as? DetailsViewController {
+                destinationVC.characterDetails = characterFeatureList[index!]
+            }
         }
     }
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(characterFeatureList.count)
-        return characterFeatureList.count
+        // print(characterFeatureList.count)
+        
+        if isSearching {
+            return resultSearch.count
+        } else {
+            return characterFeatureList.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let character = characterFeatureList[indexPath.row]
+        let character: Results
+        
+        if isSearching {
+            character = resultSearch[indexPath.row]
+        } else {
+            character = characterFeatureList[indexPath.row]
+        }
         
         guard let cell = featuresCollectionView.dequeueReusableCell(withReuseIdentifier: CharacterCollectionViewCell.identifier,
                                                                     for: indexPath) as? CharacterCollectionViewCell else {
@@ -115,5 +144,29 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         performSegue(withIdentifier: "toDetailVC", sender: indexPath.row)
+    
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Result Search: \(searchText)")
+        
+        if searchText == "" {
+            isSearching = false
+        } else {
+            isSearching = true
+
+            // resultSearch = characterFeatureList.filter({ $0.name?.lowercased().contains(searchText.lowercased()) })
+            
+            resultSearch = characterFeatureList.filter { result in
+                let containsSearchText = result.name?.lowercased().contains(searchText.lowercased()) ?? false
+                return containsSearchText
+            }
+        }
+        print(resultSearch.count)
+        
+        featuresCollectionView.reloadData()
+        
     }
 }
